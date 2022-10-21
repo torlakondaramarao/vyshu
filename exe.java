f9b7fec35e4ad65b2effca8496cddabe13f6cddc
torlakondaramarao <112272885+torlakondaramarao@users.noreply.github.com>
2022-10-21 10:46:55 +0530

    Update exe.java

--- exe.java
+++ exe.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-public class exe{
+class exe{
  public static void main(String[] args){
-  Scanner reader = new Scanner(System.in);
-  System.out.print("Enter a number: ");
-  int number = reader.nextInt();
-  System.out.println("you entered: "+number);
-  }
-  }
+  System.out.println("Git Hub!");
+  System.out.println("Welacome!!!");
+ }
+}
